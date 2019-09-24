7049639ed3b6fed9051ee4d57daf0c9a1b668bb5
Admin <conner.p.mccabe@gmail.com>
2019-09-24 10:17:21 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//muy importante, sets up the virtual DOM
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
